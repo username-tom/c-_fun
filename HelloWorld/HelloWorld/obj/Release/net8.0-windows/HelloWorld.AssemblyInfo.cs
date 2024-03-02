bb3a74b421bdaf57e5e5064062194ebacce3ef85
username-tom <tomwu3797@gmail.com>
2024-03-02 11:09:44 -0500

    add .gitignore

--- HelloWorld/HelloWorld/obj/Release/net8.0-windows/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/HelloWorld/obj/Release/net8.0-windows/HelloWorld.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f23592e059ea1a1ae1562dd53e5e7dc52b8f21")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
